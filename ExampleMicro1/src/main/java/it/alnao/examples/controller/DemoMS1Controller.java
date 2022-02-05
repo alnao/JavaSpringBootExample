67d948e7b8c9e00e644b7f9c8a75c72b25b6e576
Alberto Nao <alnao@alnao.it>
2022-02-05 18:52:14 +0100

    Logging in controller class

--- ExampleMicro1/src/main/java/it/alnao/examples/controller/DemoMS1Controller.java
+++ ExampleMicro1/src/main/java/it/alnao/examples/controller/DemoMS1Controller.java
@@ -1,5 +1,7 @@
 package it.alnao.examples.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/demoms")
 public class DemoMS1Controller {
+    Logger logger = LoggerFactory.getLogger(DemoMS1Controller.class);
+	
 	@GetMapping(value= "/response", produces= "application/json")
 	public ResponseEntity<String> response(){
+		logger.debug("response api called");
 		return new ResponseEntity<String> ("{\"response\":\"ok\"}",HttpStatus.OK)  ;
 	}
 	//http://localhost:5051/api/demoms/response
