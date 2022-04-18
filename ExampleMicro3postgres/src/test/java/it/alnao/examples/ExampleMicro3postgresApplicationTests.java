2b57dba0b063c26619526d390a516a795dfe2ae5
Alberto Nao <alnao@alnao.it>
2022-04-18 12:56:53 +0200

    ExampleMicro3postgresApplication

---
+++ ExampleMicro3postgres/src/test/java/it/alnao/examples/ExampleMicro3postgresApplicationTests.java
@@ -0,0 +1,13 @@
+package it.alnao.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMicro3postgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
