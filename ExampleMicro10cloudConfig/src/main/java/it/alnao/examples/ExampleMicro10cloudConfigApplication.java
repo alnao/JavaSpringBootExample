017889549808c67413ecc993202db8500a49d64f
Alberto Nao <alnao@alnao.it>
2022-10-31 14:34:20 +0100

    ExampleMicro10cloudConfig progetto CloudConfig

---
+++ ExampleMicro10cloudConfig/src/main/java/it/alnao/examples/ExampleMicro10cloudConfigApplication.java
@@ -0,0 +1,16 @@
+package it.alnao.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ExampleMicro10cloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMicro10cloudConfigApplication.class, args);
+	} 
+
+}
