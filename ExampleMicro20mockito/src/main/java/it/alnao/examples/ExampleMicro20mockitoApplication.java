e53dd1e2945ee7f7e7ba2cfff749c9206723be91
Alberto Nao <alnao@alnao.it>
2023-04-20 15:40:31 +0200

    POM and @EnableAutoConfiguration

--- ExampleMicro20mockito/src/main/java/it/alnao/examples/ExampleMicro20mockitoApplication.java
+++ ExampleMicro20mockito/src/main/java/it/alnao/examples/ExampleMicro20mockitoApplication.java
@@ -4,10 +4,12 @@ import java.util.Date;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
+@EnableAutoConfiguration
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ExampleMicro20mockitoApplication{
 	
