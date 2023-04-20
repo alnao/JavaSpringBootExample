9ec7c51f2f49075bee65218f6cc1fad224e9a88a
Alberto Nao <alnao@alnao.it>
2023-04-20 16:32:30 +0200

    ExampleMicro20mockitoConfiguration

---
+++ ExampleMicro20mockito/src/main/java/it/alnao/examples/ExampleMicro20mockitoConfiguration.java
@@ -0,0 +1,8 @@
+package it.alnao.examples;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExampleMicro20mockitoConfiguration {
+	//https://www.baeldung.com/spring-boot-custom-auto-configuration
+}
